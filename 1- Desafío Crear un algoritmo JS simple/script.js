b67478d805d7aee249edb7477d2f496266eda456
Agustin Ricardo <agustinricardo14@gmail.com>
2021-01-08 11:27:31 -0300

    Primer desafio

---
+++ 1- Desafío Crear un algoritmo JS simple/script.js
@@ -0,0 +1,11 @@
+var numero = 0;
+
+numero = prompt('Indique su numero');
+
+if ((numero %2) == 0) {
+    alert(numero + ' es par');
+    console.log(numero + ' es par');
+} else {
+    alert(numero + ' es impar')
+    console.log(numero + ' es impar');
+}
